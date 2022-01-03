adb26b5d20c1381e6b33cc17506e523bb883a586
Lara Balsamello <lara.balsamello@gmail.com>
2022-01-03 20:19:54 -0300

    chore: basic logic to find neighbours

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,92 @@
-import React from 'react';
+/* eslint-disable react/jsx-curly-newline */
+import React, { FunctionComponent, useState } from 'react';
 import './App.scss';
 import Cel from './components';
+import {
+  getBottomNeighbour,
+  getLeftNeighbour,
+  getLeftSubNeighbour,
+  getRightNeighbour,
+  getTopNeighbour,
+  getRightSubNeighbour,
+} from './core/utils';
+import { GetNeighBoursProps, Neighbours } from './types';
 
-const App = () => {
-  const dashboard = { columns: 10, rows: 10 };
+const App: FunctionComponent = () => {
+  const dashboard = { columns: 50, rows: 30 };
+  const [paintIndex, setPaintIndexes] = useState<Array<number>>();
+
+  const getNeighbours = ({
+    currentCel,
+    celsPerRow,
+    numberOfRows,
+    indexInRow,
+  }: GetNeighBoursProps): void => {
+    const cellNeighbours: Neighbours = {
+      bottom: getBottomNeighbour(
+        currentCel,
+        celsPerRow,
+        numberOfRows,
+        indexInRow,
+      ),
+      top: getTopNeighbour(currentCel, celsPerRow, numberOfRows, indexInRow),
+      left: getLeftNeighbour(currentCel, celsPerRow, indexInRow),
+      right: getRightNeighbour(currentCel, celsPerRow, indexInRow),
+      bottomLeft: getLeftSubNeighbour(
+        getBottomNeighbour(currentCel, celsPerRow, numberOfRows, indexInRow),
+        indexInRow,
+        celsPerRow,
+      ),
+      bottomRight: getRightSubNeighbour(
+        getBottomNeighbour(currentCel, celsPerRow, numberOfRows, indexInRow),
+        indexInRow,
+        celsPerRow,
+      ),
+      topLeft: getLeftSubNeighbour(
+        getTopNeighbour(currentCel, celsPerRow, numberOfRows, indexInRow),
+        indexInRow,
+        celsPerRow,
+      ),
+      topRight: getRightSubNeighbour(
+        getTopNeighbour(currentCel, celsPerRow, numberOfRows, indexInRow),
+        indexInRow,
+        celsPerRow,
+      ),
+    };
+    setPaintIndexes([
+      cellNeighbours.top,
+      cellNeighbours.bottom,
+      cellNeighbours.left,
+      cellNeighbours.right,
+      cellNeighbours.bottomLeft,
+      cellNeighbours.bottomRight,
+      cellNeighbours.topLeft,
+      cellNeighbours.topRight,
+    ]);
+  };
+
   return (
     <div className="dashboard">
-      {[...Array(dashboard.rows)].map(() => (
+      {[...Array(dashboard.rows)].map((x, z) => (
         <div className="dashboard-row">
           {[...Array(dashboard.columns)].map((v, i) => (
             // eslint-disable-next-line react/no-array-index-key
-            <Cel key={`cel${i}`} />
+            <Cel
+              onClickHandler={() => {
+                getNeighbours({
+                  currentCel: i + dashboard.columns * z,
+                  celsPerRow: dashboard.columns,
+                  numberOfRows: dashboard.rows,
+                  indexInRow: i,
+                });
+              }}
+              paintElement={
+                paintIndex?.some(
+                  (value) => value === i + dashboard.columns * z,
+                ) ?? false
+              }
+              key={`cel${i + z}`}
+            />
           ))}
         </div>
       ))}
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface Neighbours {
+  topRight: number;
+  top: number;
+  topLeft: number;
+  right: number;
+  left: number;
+  bottomRight: number;
+  bottom: number;
+  bottomLeft: number;
+}
+
+export interface GetNeighBoursProps {
+  currentCel: number;
+  celsPerRow: number;
+  numberOfRows: number;
+  indexInRow: number;
+}
